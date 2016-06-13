f9c827ace4be0419adb8094f61baebd15895e1ab
WebCrew <palareas@gmail.com>
2016-06-14 00:12:39 +0200

    Update adframe.php

--- widgets/views/adframe.php
+++ widgets/views/adframe.php
@@ -9,10 +9,10 @@ use humhub\models\Setting;
   <div class="panel-body">
   <div id="slideshow">
    <div>
-     <img src="//farm6.static.flickr.com/5224/5658667829_2bb7d42a9c_m.jpg">
+     <img style="width: 100%" src="//farm6.static.flickr.com/5224/5658667829_2bb7d42a9c_m.jpg">
    </div>
    <div>
-     <img src="//farm6.static.flickr.com/5230/5638093881_a791e4f819_m.jpg">
+     <img style="width: 100%" src="//farm6.static.flickr.com/5230/5638093881_a791e4f819_m.jpg">
    </div>
    <div>
      Pretty cool eh? Integrate any content into the slider.
