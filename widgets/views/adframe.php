83df2847df3e22db9caeb8987f9b4b70b06100d4
WebCrew <palareas@gmail.com>
2016-06-16 23:29:23 +0200

    Update
    I reworked the whole slider, it is using HumHubs default Bootstrap and minor CSS now.

--- widgets/views/adframe.php
+++ widgets/views/adframe.php
@@ -7,16 +7,43 @@ use humhub\models\Setting;
     <?=Yii::t('AdspanelModule.base', '<strong>Ads</strong> Panel'); ?>
   </div>
   <div class="panel-body">
-  <div id="slideshow">
-   <div>
-     <img style="width: 100%" src="//farm6.static.flickr.com/5224/5658667829_2bb7d42a9c_m.jpg">
-   </div>
-   <div>
-     <img style="width: 100%" src="//farm6.static.flickr.com/5230/5638093881_a791e4f819_m.jpg">
-   </div>
-   <div>
-     Pretty cool eh? Integrate any content into the slider.
-   </div>
-</div>
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="http://placehold.it/800x400" alt="Image 1">
+          <div class="carousel-caption">
+            <h2>Demo 1</h2>
+          </div>
+        </div>
+        <div class="item">
+          <img src="http://placehold.it/800x400" alt="Image 2">
+          <div class="carousel-caption">
+            <h2>Demo 2</h2>
+          </div>
+        </div>
+        <div class="item">
+          <img src="http://placehold.it/800x400" alt="Image 3">
+          <div class="carousel-caption">
+            <h2>Demo 3</h2>
+          </div>
+        </div>
+      </div>
+
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+    </div>
 </div>
 </div>
