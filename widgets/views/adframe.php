3c751c618288847131a02ac1d40e47670e6b4b47
WebCrew <palareas@gmail.com>
2016-06-18 01:24:14 +0200

    New
    a lot got changed and I made it easier

--- widgets/views/adframe.php
+++ widgets/views/adframe.php
@@ -7,76 +7,30 @@ use humhub\models\Setting;
     <?=Yii::t('AdspanelModule.base', '<strong>Ads</strong> Panel'); ?>
   </div>
   <div class="panel-body">
-  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="5"></li>
-      </ol>
+      
+         <a href="http://www.drakensang.com/?aid=1695&invID=xbegegiechbxcifbaggfgxbagaghbigxeibxbgjfxbab&force_instance=481&lang=de" target="_blank">
+          <img class="mySlides" src="http://todgmbh.de/cms/ads/1.jpg" style="width: 100%; height: auto;" alt="Play Drakensang">
+          </a>
+          
+         <a href="http://www.drakensang.com/?aid=1695&invID=xbegegiechbxcifbaggfgxbagaghbigxeibxbgjfxbab&force_instance=481&lang=de" target="_blank">
+          <img class="mySlides" src="http://todgmbh.de/cms/ads/2.jpg" style="width: 100%; height: auto;"  alt="Drakensang Community">
+          </a>
+         
+         <a href="http://web-crew.org" target="_blank">
+          <img class="mySlides" src="http://todgmbh.de/cms/ads/3.jpg" style="width: 100%; height: auto;"  alt="WebCrew Webdesign">
+          </a>
+         
+         <a href="http://humhub.com" target="_blank">
+          <img class="mySlides" src="http://todgmbh.de/cms/ads/4.jpg" style="width: 100%; height: auto;"  alt="HumHub CMS">
+          </a>
+         
+         <a href="https://twitter.com/The_TodGmbH" target="_blank"> 
+          <img class="mySlides" src="http://todgmbh.de/cms/ads/5.jpg" style="width: 100%; height: auto;"  alt="Twitter TodGmbH">
+          </a>
+         
+         <a href="mailto:palareas@gmail.com" target="_blank">
+          <img class="mySlides" src="http://todgmbh.de/cms/ads/6.jpg" style="width: 100%; height: auto;"  alt="Your Ad here!">
+          </a>
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item active">
-         <a href="http://humhub.com" target="_blank">
-          <img src="http://placehold.it/800x400" alt="Image 1">
-          <div class="carousel-caption">
-            <h2></h2>
-            </a>
-          </div>
-        </div>
-        <div class="item">
-         <a href="http://humhub.com" target="_blank">
-          <img src="http://placehold.it/800x400" alt="Image 2">
-          <div class="carousel-caption">
-            <h2></h2>
-            </a>
-          </div>
-        </div>
-        <div class="item">
-         <a href="http://humhub.com" target="_blank">
-          <img src="http://placehold.it/800x400" alt="Image 3">
-          <div class="carousel-caption">
-            <h2></h2>
-            </a>
-          </div>
-        </div>
-        <div class="item">
-         <a href="http://todgmbh.de" target="_blank">
-          <img src="http://placehold.it/800x400" alt="Image 4">
-          <div class="carousel-caption">
-            <h2></h2>
-            </a>
-          </div>
-        </div>
-        <div class="item">
-         <a href="http://humhub.com" target="_blank"> 
-          <img src="http://placehold.it/800x400" alt="Image 5">
-          <div class="carousel-caption">
-            <h2></h2>
-            </a>
-          </div>
-        </div>
-        <div class="item">
-         <a href="http://humhub.com" target="_blank">
-          <img src="http://placehold.it/800x400" alt="Image 6">
-          <div class="carousel-caption">
-            <h2></h2>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <!-- Controls -->
-      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-      </a>
-    </div>
 </div>
 </div>
