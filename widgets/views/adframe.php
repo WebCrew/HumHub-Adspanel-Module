b811380e84b3a934e5e921a9cb87b2d5f706e2b6
WebCrew <palareas@gmail.com>
2016-06-17 19:24:51 +0200

    Updated
    I gave the images now also some links, so You can edit them like You need.
    It fits better with the Ads.

--- widgets/views/adframe.php
+++ widgets/views/adframe.php
@@ -13,26 +13,59 @@ use humhub\models\Setting;
         <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
         <li data-target="#carousel-example-generic" data-slide-to="1"></li>
         <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="5"></li>
       </ol>
 
       <!-- Wrapper for slides -->
       <div class="carousel-inner">
         <div class="item active">
+         <a href="http://humhub.com" target="_blank">
           <img src="http://placehold.it/800x400" alt="Image 1">
           <div class="carousel-caption">
-            <h2>Demo 1</h2>
+            <h2></h2>
+            </a>
           </div>
         </div>
         <div class="item">
+         <a href="http://humhub.com" target="_blank">
           <img src="http://placehold.it/800x400" alt="Image 2">
           <div class="carousel-caption">
-            <h2>Demo 2</h2>
+            <h2></h2>
+            </a>
           </div>
         </div>
         <div class="item">
+         <a href="http://humhub.com" target="_blank">
           <img src="http://placehold.it/800x400" alt="Image 3">
           <div class="carousel-caption">
-            <h2>Demo 3</h2>
+            <h2></h2>
+            </a>
+          </div>
+        </div>
+        <div class="item">
+         <a href="http://todgmbh.de" target="_blank">
+          <img src="http://placehold.it/800x400" alt="Image 4">
+          <div class="carousel-caption">
+            <h2></h2>
+            </a>
+          </div>
+        </div>
+        <div class="item">
+         <a href="http://humhub.com" target="_blank"> 
+          <img src="http://placehold.it/800x400" alt="Image 5">
+          <div class="carousel-caption">
+            <h2></h2>
+            </a>
+          </div>
+        </div>
+        <div class="item">
+         <a href="http://humhub.com" target="_blank">
+          <img src="http://placehold.it/800x400" alt="Image 6">
+          <div class="carousel-caption">
+            <h2></h2>
+            </a>
           </div>
         </div>
       </div>
