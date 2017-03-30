bcf3cbfcff85c02684e4f08cd4806f167996dc05
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-30 16:02:16 -0400

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,22 +1,23 @@
 <?php
-use humhub\compat\CActiveForm;
-use humhub\compat\CHtml;
+
+use yii\bootstrap\ActiveForm;
+use yii\helper\Html;
 use humhub\models\Setting;
 use humhub\modules\adspanel\controllers\AdminController;
 ?>
 <div class="panel panel-default">
-	<div class="panel-heading"><?=Yii::t('AdspanelModule.base', '<strong>Adspanel</strong>'); ?></div>
+	<div class="panel-heading"><?= Yii::t('AdspanelModule.base', '<strong>Adspanel</strong>'); ?></div>
 	<div class="panel-body">
-		<?php $form = CActiveForm::begin(['id' => 'adspanel-settings-form']); ?>
-			<?=$form->errorSummary($model); ?>
-			<p class="help-block"><?=Yii::t('AdspanelModule.base', 'eg:  "99999999"'); ?></p>
+		<?php $form = ActiveForm::begin(['id' => 'adspanel-settings-form']); ?>
+			<?= $form->errorSummary($model); ?>
+			<p class="help-block"><?= Yii::t('AdspanelModule.base', 'eg:  "99999999"'); ?></p>
 			<div class="form-group">
-				<?=$form->labelEx($model, 'sort'); ?>
-				<?=$form->textField($model, 'sort', ['class' => 'form-control', 'readonly' => Setting::IsFixed('sort', 'adspanel')]); ?>
+				<?= $form->labelEx($model, 'sort'); ?>
+				<?= $form->textField($model, 'sort', ['class' => 'form-control', 'readonly' => Setting::IsFixed('sort', 'adspanel')]); ?>
 			</div>
-			<p class="help-block"><?=Yii::t('AdspanelModule.base', 'Widget positioning.'); ?></p>
-			<?= CHtml::submitButton(Yii::t('AdspanelModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
-			<?=\humhub\widgets\DataSaved::widget(); ?>
-		<?php CActiveForm::end(); ?>
+			<p class="help-block"><?= Yii::t('AdspanelModule.base', 'Widget positioning.'); ?></p>
+			<?= Html::submitButton(Yii::t('AdspanelModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
+			<?= \humhub\widgets\DataSaved::widget(); ?>
+		<?php ActiveForm::end(); ?>
 	</div>
 </div>
